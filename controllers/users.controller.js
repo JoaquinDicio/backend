98aad638678b220fd8c4210045e2187351243e1d
joaquin <joacodicio@gmail.com>
2023-01-09 21:13:56 -0300

    proyecto con validaciones de datos

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,5 @@
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 const Usuario = require('../models/usuarios')
 
 function getUsers( req,res ){
@@ -7,8 +9,24 @@ function getUsers( req,res ){
 }
 async function postUsers( req,res ){
 
-    const body= req.body;
-    const usuario = new Usuario(body); //creating a new instance
+    //desestructuring user object
+    const {nombre,correo,password,rol}= req.body;
+    const usuario = new Usuario({nombre,password,rol,correo}); //creating a new instance
+    
+    //watching for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json(errors)
+
+    //looking for the same email in db
+    const coincidence = await Usuario.findOne({ correo:correo })
+    if (coincidence) return res.status(400).json({
+        msg:'El correo ya esta registrado'
+    })
+    //encrypting
+    const salt = bcryptjs.genSaltSync() //this number defines the difficult of the encrypt system
+    usuario.password = bcryptjs.hashSync( password.toString(), salt ) //encrypt the password
+    
+    //saving new user on db
     await usuario.save()
     res.send(usuario)
 
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express')
+const {check,isEmail} = require('express-validator')
 const { getUsers,postUsers,deleteUsers,putUsers } = require('../controllers/users.controller')
 const router = Router ()
 
 router.get('/', getUsers )
-router.post('/', postUsers )
+router.post('/',[
+    check('correo','No es un correo valido').isEmail(),
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','La contraseña debe ser mayor a 6 caracteres').isLength({min:6}),
+    check('rol','No es un rol valido').isIn(['admin','user'])
+], postUsers ) //con tres argumentos, el del medio es middleware
 router.put('/:id', putUsers) //recibe el parametro id
 router.delete('/:id', deleteUsers) //recibe el parametro id
 
